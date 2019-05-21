f1cadca23c0e508f3bb43baab35018535e312a97
MicaelaLagana <lagana.mcl@gmail.com>
2019-05-21 17:12:16 -0300

    First commit

---
+++ ej1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dir = new string[] {"n","s","n","s","n","s","n","s","n","s"};
+            string[] dir1 = new string[] {"n","s","s","s","n","s","n","s"};
+
+            isValid(dir);
+            esValido(dir);
+            Console.WriteLine(isValid(dir));
+            Console.WriteLine(isValid(dir1));
+
+
+            bool isValid (string[] recorrido){
+                int x=0;
+                int y=0;
+                    for (int i=0; i<recorrido.Length; i++ ){
+                            if( recorrido[i] == "n"){
+                                x++;
+                            }else if(recorrido[i]=="s"){
+                                x--;
+                            }else if(recorrido[i]=="w"){
+                                y--;
+                            }else if(recorrido[i]=="e"){
+                                y++;
+                            }
+                    }
+                    if ( (recorrido.Length == 10) && (x == 0) && (y == 0) ){
+                       return true; 
+                    }else{
+                        return false;
+                    }
+            }
+
+
+
+        }
+    }
+    
+}
---
+++ ej2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(XO("asdasxasdasdoasdasdXxasdasdoO0asdx"));
+            Console.WriteLine(XO("xxoO"));
+            bool XO(string input){
+                
+                bool res = false;
+
+                int x=0;
+            
+                for(int i=0; i< input.Length; i++ ){
+                    if(Char.ToLower(input[i]) == 'o'){
+                        x++;
+                    } else if(Char.ToLower(input[i]) == 'x'){
+                    
+                        x--;                
+                    }
+                }
+                
+                if(x==0){
+                    res = true;
+                }
+                return res;
+            }
+        }
+    }
+}

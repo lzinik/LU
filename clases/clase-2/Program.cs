fc910da6da17b9c4b17667d165fac1b9a9481d49
Luciano Zinik <lzinik@gmail.com>
2019-05-22 09:42:00 -0300

    Second Commit

---
+++ clases/clase-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var suma = 0.0m; //Definir variable sin decirle el tipo
+        }
+    }
+}
